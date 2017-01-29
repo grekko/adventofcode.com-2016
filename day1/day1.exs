adb1c4fa784224d9a6460c96d0fe5a3bf0297134
Gregory Igelmund <gregory.igelmund@gmail.com>
2017-01-29 14:55:00 +0100

    Day1: Add docs #2

--- day1/day1.exs
+++ day1/day1.exs
@@ -16,31 +16,38 @@ defmodule ElfHQ do
   # entry-point
   # 1. Reads given input file, returns BitString <<82, 42, …>>
   # 2. Splits BitString into List of BitStrings ["R1", "L1", …]
-  # 3. Delegates to walk
+  # 3. Delegates to walk with initial position and orientation
   def run do
     { :ok, input } = File.read("input.txt")
     movements = String.split(input, ", ")
-    walk(movements)
+    walk([ movements, [ 0, 0, :north ]])
   end
 
+  # final step
+  # Thw movements list is empty, so we reached our target.
   def walk([[], [ x, y, orientation ]]) do
-    IO.puts "You arrived at: #{x}, #{y} looking towards #{orientation}. So the distance is #{x+y}"
+    IO.puts "Arrived at: #{x}, #{y} looking towards #{orientation}. So the distance is #{x+y}"
   end
 
+  # processing the movements
+  # 1. Fetch the next movement from the head of the list
+  # 2. Extract the rotation and distance (steps to move)
+  # 3. Calculate new_orientation and apply distance to calculate new position
+  # 4. Call the same method with the new_orientation, updated position and tail of movements
   def walk([[ movement | tail ], [ x, y, orientation ]]) do
-    IO.inspect(movement)
     <<rotate_to::8, distance_binary::binary>> = movement
+    # :string.to_integer is an Erlang function that translates a char list (List of codepoints)
+    # to the integer it represents. The second return value is a rest in case of a decimal value, e.g. "0.5"
+    # Other examples:
+    # :string.to_integer('3.1415')
+    # {3, '.1415'}
+    # :string.to_integer('-35')
+    # {-35, []}
     { distance, _ } = :string.to_integer(to_char_list(distance_binary))
     new_orientation = navigate(orientation, rotate_to)
     { x1, y1 } = move(new_orientation, x, y, distance)
-    IO.puts("Looking #{orientation}, turning #{new_orientation}. Going by #{distance}")
-    IO.puts("Moving from #{x},#{y} to #{x1}, #{y1}")
+    IO.puts("Turning #{orientation} -> #{new_orientation}. Taking #{distance} step(s) from [#{x}, #{y}] to [#{x1}, #{y1}]")
     walk([tail, [ x1, y1, new_orientation ]])
-  end
-
-  def walk(movements) do
-    # walk([ movements ], [ 0, 0, :north ])
-    walk([ movements, [ 0, 0, :north ]])
   end
 
   defp move(orientation, x, y, distance) do
