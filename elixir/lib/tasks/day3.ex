6e2ccc73e4b3e226f749f5a6f5d16304e14e8268
Gregory Igelmund <gregory.igelmund@gmail.com>
2017-01-31 21:12:17 +0100

    Day3: Add solution for 2nd riddle

--- elixir/lib/tasks/day3.ex
+++ elixir/lib/tasks/day3.ex
@@ -5,12 +5,35 @@ defmodule Mix.Tasks.Day3 do
     { :ok, input } = File.read("inputs/day3.txt")
     triangle_lines = String.split(input, "\n")
     maybe_triangles = Enum.map(triangle_lines, fn(x) -> parse_triangle(x) end)
+    vertical_triangles = generate_vertical_triangles(maybe_triangles)
     real_triangles = reject_invalid_triangles(maybe_triangles)
+    real_vertical_triangles = reject_invalid_triangles(vertical_triangles)
 
     IO.puts("---------------------------------")
-    IO.puts("Triangles: #{real_triangles}")
     IO.puts("Count: #{length(real_triangles)}")
+    IO.puts("Vertical: #{length(real_vertical_triangles)}")
     IO.puts("---------------------------------")
+  end
+
+  @doc """
+  Transforms horizontal triangles to vertical triangles.
+
+  ## Examples
+
+      iex> Mix.Tasks.Day3.generate_vertical_triangles([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+      [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+
+  """
+  def generate_vertical_triangles(triangles) do
+    pair_vertical_triangles(List.flatten(triangles), [])
+  end
+
+  defp pair_vertical_triangles([a, b, c, d, e, f, g, h, i | rest], results) do
+    pair_vertical_triangles(rest, results ++ [[a, d, g], [b, e, h], [c, f, i]])
+  end
+
+  defp pair_vertical_triangles([], results) do
+    results
   end
 
   @doc """
